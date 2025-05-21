9bab0353709cf8f927a685c4447b947f256bd8c9
aabccd021 <aabccd021@gmail.com>
2025-05-21 10:51:30 +0700

    empty

--- secrets/default.nix
+++ secrets/default.nix
@@ -6,7 +6,7 @@
   ed25519.alice.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWpfDJ/8B3QpB2qUmgFio0TaV0ulAaNc0eKAxC5FiK/";
   ed25519.alice.private = ./ed25519-alice.txt;
 
-
   ed25519.bob.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEy2bFKXQMLypMyNYIZSBwsSUzDrD6YmBWoF99M5It4x";
   ed25519.bob.private = ./ed25519-bob.txt;
+
 }
