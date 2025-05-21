46a81f8300d956feb7cf9397876495149084f5cd
aabccd021 <aabccd021@gmail.com>
2025-05-21 11:04:58 +0700

    Delete SSH keys for Alice and Bob, update `default.nix` with new private keys.

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,12 +1,29 @@
 {
 
   age.alice.public = "age1hmcl6rhw6y7pe4tl5a4c3g3vp6rh0zg0w54qgeq7xem3v8c3rs8se6p6n2";
-  age.alice.private = ./age-alice.txt;
+  age.alice.private = "AGE-SECRET-KEY-12SKJ26502P40W9XG5Y2Y0Y7FTUME9CXC0W2QMEMYARQC7PN48WGQ5M4RDU";
 
   ed25519.alice.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWpfDJ/8B3QpB2qUmgFio0TaV0ulAaNc0eKAxC5FiK/";
-  ed25519.alice.private = ./ed25519-alice.txt;
+  ed25519.alice.private = ''
+    -----BEGIN OPENSSH PRIVATE KEY-----
+    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+    QyNTUxOQAAACClqXwyf/Ad0KQdqlJoBYqNE2ldLpQGjXNHigMQuRYivwAAAJiTUdVlk1HV
+    ZQAAAAtzc2gtZWQyNTUxOQAAACClqXwyf/Ad0KQdqlJoBYqNE2ldLpQGjXNHigMQuRYivw
+    AAAEAA/W+k/Uecrh5L6AgzkmJxIv04WbJkp7i9SO3gmpKRDKWpfDJ/8B3QpB2qUmgFio0T
+    aV0ulAaNc0eKAxC5FiK/AAAAEWFhYmNjZDAyMUBkZXNrdG9wAQIDBA==
+    -----END OPENSSH PRIVATE KEY-----
+  '';
 
   ed25519.bob.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEy2bFKXQMLypMyNYIZSBwsSUzDrD6YmBWoF99M5It4x";
-  ed25519.bob.private = ./ed25519-bob.txt;
+  ed25519.bob.private =
+    ''
+      -----BEGIN OPENSSH PRIVATE KEY-----
+      b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+      QyNTUxOQAAACBMtmxSl0DC8qTMjWCGUgcLElMw6w+mJgVqBffTOSLeMQAAAJhcpeAtXKXg
+      LQAAAAtzc2gtZWQyNTUxOQAAACBMtmxSl0DC8qTMjWCGUgcLElMw6w+mJgVqBffTOSLeMQ
+      AAAECl78pyKT6/Scktz8gPBd9f9XIN1iG98MSJlnX0wKI9Aky2bFKXQMLypMyNYIZSBwsS
+      UzDrD6YmBWoF99M5It4xAAAAEWFhYmNjZDAyMUBkZXNrdG9wAQIDBA==
+      -----END OPENSSH PRIVATE KEY-----
+    '';
 
 }
