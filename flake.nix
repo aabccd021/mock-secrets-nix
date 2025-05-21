120dc714ec90f484756f61b3b9014815b08bcf8b
aabccd021 <aabccd021@gmail.com>
2025-05-21 10:03:53 +0700

    Empty

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+
+  nixConfig.allow-import-from-derivation = false;
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.sops-nix.url = "github:Mic92/sops-nix";
+
+
+  outputs = { self, ... }@inputs:
+    let
+
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+
+      treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.nixpkgs-fmt.enable = true;
+      };
+
+      formatter = treefmtEval.config.build.wrapper;
+
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.nixd ];
+      };
+
+      packages = devShells // {
+        formatting = treefmtEval.config.build.check self;
+        formatter = formatter;
+      };
+
+    in
+
+    {
+
+      packages.x86_64-linux = packages // rec {
+        gcroot = pkgs.linkFarm "gcroot" packages;
+        default = gcroot;
+      };
+
+      checks.x86_64-linux = packages;
+      formatter.x86_64-linux = formatter;
+      devShells.x86_64-linux = devShells;
+
+    };
+}
