0dd838ac7b2a8f313adada6c06fb5654bfd29016
aabccd021 <aabccd021@gmail.com>
2025-05-21 10:27:07 +0700

    feat(secrets): Add SSH key pairs for Alice and Bob

--- secrets/default.nix
+++ secrets/default.nix
@@ -3,4 +3,10 @@
   age.alice.public = "age1hmcl6rhw6y7pe4tl5a4c3g3vp6rh0zg0w54qgeq7xem3v8c3rs8se6p6n2";
   age.alice.private = ./age-alice.txt;
 
+  ed25519.alice.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWpfDJ/8B3QpB2qUmgFio0TaV0ulAaNc0eKAxC5FiK/";
+  ed25519.alice.private = ./ed25519-alice.txt;
+
+
+  ed25519.bob.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEy2bFKXQMLypMyNYIZSBwsSUzDrD6YmBWoF99M5It4x";
+  ed25519.bob.private = ./ed25519-bob.txt;
 }
