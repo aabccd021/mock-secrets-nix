e8a0d225d6527aaa651027f07008154d935e2603
aabccd021 <aabccd021@gmail.com>
2025-09-17 07:29:14 +0700

    Revert "- Remove unused files and update dependencies"
    This reverts commit 80744ba3df02bd1fe3fd57a25f29be65eb8c11a1.

--- flake.nix
+++ flake.nix
@@ -4,65 +4,115 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.sops-nix.url = "github:Mic92/sops-nix";
 
   outputs =
     { self, ... }@inputs:
     let
+      lib = inputs.nixpkgs.lib;
+
+      collectInputs =
+        is:
+        pkgs.linkFarm "inputs" (
+          builtins.mapAttrs (
+            name: i:
+            pkgs.linkFarm name {
+              self = i.outPath;
+              deps = collectInputs (lib.attrByPath [ "inputs" ] { } i);
+            }
+          ) is
+        );
 
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+
+      libs.secrets = import ./secrets;
+
+      overlays.default = final: prev: {
+        mock-secrets = libs.secrets;
+      };
 
       treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = true;
-        programs.prettier.enable = true;
       };
 
-      packages.formatting = treefmtEval.config.build.check self;
+      formatter = treefmtEval.config.build.wrapper;
 
-      packages.generate-age = pkgs.writeShellApplication {
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.nixd ];
+      };
+
+      scripts.generate-age = pkgs.writeShellApplication {
         name = "generate-age";
-        runtimeInputs = [
-          pkgs.age
-          pkgs.jq
-          pkgs.moreutils
-        ];
+        runtimeInputs = [ pkgs.age ];
         text = ''
-          json_file="$(git rev-parse --show-toplevel)/secrets.json"
+          if [ $# -ne 1 ]; then
+            echo "Error: Please provide an identifier as argument"
+            echo "Usage: $0 <identifier>"
+            exit 1
+          fi
 
-          private_key=$(age-keygen)
-          public_key=$(printf "%s" "$private_key" | age-keygen -y)
+          trap 'cd $(pwd)' EXIT
+          repo_root=$(git rev-parse --show-toplevel)
+          cd "$repo_root" || exit
 
-          jq --arg id "$1" --arg private "$private_key" --arg public "$public_key" \
-          '.age[$id] = {private: $private, public: $public}' "$json_file" | sponge "$json_file"
+          identifier="$1"
+
+          private_key_file="./secrets/age-$identifier-private.txt"
+          public_key_file="./secrets/age-$identifier-public.txt"
+
+          age-keygen -o "$private_key_file"
+          age-keygen -y "$private_key_file" > "$public_key_file"
         '';
       };
 
-      packages.generate-ed25519 = pkgs.writeShellApplication {
+      scripts.generate-ed25519 = pkgs.writeShellApplication {
         name = "generate-ed25519";
-        runtimeInputs = [
-          pkgs.openssh
-          pkgs.jq
-          pkgs.moreutils
-        ];
+        runtimeInputs = [ pkgs.openssh ];
         text = ''
-          json_file="$(git rev-parse --show-toplevel)/secrets.json"
+          if [ $# -ne 1 ]; then
+            echo "Error: Please provide an identifier as argument"
+            echo "Usage: $0 <identifier>"
+            exit 1
+          fi
 
-          tmpdir=$(mktemp -d)
-          trap 'rm -rf "$tmpdir"' EXIT
-          cd "$tmpdir" || exit
+          trap 'cd $(pwd)' EXIT
+          repo_root=$(git rev-parse --show-toplevel)
+          cd "$repo_root" || exit
 
-          ssh-keygen -t ed25519 -f ./temp_key -N "" -C "$1" -q
+          identifier="$1"
 
-          jq --arg id "$1" --arg private "$(cat ./temp_key)" --arg public "$(cat ./temp_key.pub)" \
-          '.ed25519[$id] = {private: $private, public: $public}' "$json_file" | sponge "$json_file"
+          private_key_file="./secrets/ed25519-$identifier-private.txt"
+          public_key_file="./secrets/ed25519-$identifier-public.txt"
+
+          ssh-keygen -t ed25519 -f "$private_key_file" -N "" -C "$identifier" -q
+          mv "$private_key_file.pub" "$public_key_file"
         '';
       };
+
+      packages =
+        scripts
+        // devShells
+        // {
+          formatting = treefmtEval.config.build.check self;
+          formatter = formatter;
+          allInputs = collectInputs inputs;
+        };
 
     in
 
     {
-      packages.x86_64-linux = packages;
+
+      packages.x86_64-linux = packages // rec {
+        gcroot = pkgs.linkFarm "gcroot" packages;
+        default = gcroot;
+      };
+
       checks.x86_64-linux = packages;
-      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+      formatter.x86_64-linux = formatter;
+      devShells.x86_64-linux = devShells;
+      lib = libs;
+      overlays = overlays;
+
     };
 }
